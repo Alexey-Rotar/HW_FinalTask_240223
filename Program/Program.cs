a4252a4c6119f573d295a157ad7b0fbf33a45088
alexey.rotar <ar_mail@inbox.ru>
2023-02-24 14:27:29 +0500

    Описаны методы ввода массива и основной метод CreatingLimitedArray

--- Program/Program.cs
+++ Program/Program.cs
@@ -2,3 +2,36 @@
 // длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Ввод первоначального массива
+string[] CreatingArray()
+{
+    string input;
+    string[] array;
+    input = Console.ReadLine()!;
+    array = input.Split(" ");
+    return array;
+}
+
+// Формирование массива из строк, длина которых меньше либо равна 3 символа
+string[] CreatingLimitedArray(string[] array)
+{
+    int count = 0, j = 0;
+    for (int i = 0; i < array.Length; ++i)
+    {
+        if (array[i].Length < 4)
+            count++;
+    }
+    
+    string[] limitedArray = new string[count];
+
+    for (int i = 0; i < array.Length; ++i)
+    {
+        if (array[i].Length < 4)
+            {
+                limitedArray[j] = array[i];
+                j++;
+            }
+    }
+    return limitedArray;
+}
